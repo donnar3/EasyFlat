b26160ddcb592af84ecdc09ac63ea45e151c33fe
Tadija-Skugor <tadija.skugor@fer.unizg.hr>
2024-10-23 14:25:06 +0200

    Delete frontend directory

--- frontend/src/components/Footer.js
+++
@@ -1,6 +0,0 @@
-export default function Footer(){
-  
-    return(
-<p>Ovo je footer od stranice i moze biti stogod</p>
-    )
-    }
--- frontend/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-export default function Home(){
-return (
-    <>
-        <h1>Login Stranica</h1>
-        <p>Ovdje cemo dodati login innpute kakve su bili napravljeni na figmi</p>
-    </>
-)
-
-} 
